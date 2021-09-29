ca05953d988c3d8d4fc317c128e1441df0b97210
Jose Nortes <nooors@gmail.com>
2021-09-29 16:26:15 +0200

    Rating stars runnig with Javascript

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-const stars = document.getElementsByName("i");
-const elementListenerParent = document.querySelector(".container");
+// ... "spread operator" means every element of the list
+const ratingStars = [...document.getElementsByClassName("star__icon")];
+const container = document.getElementById("container");
 
-// The listener added to a parent will be spread to all his children (or not?)
-elementListenerParent.addEventListener ("mouseover", (e) => {
-    console.log(e);
-    console.log(e.target);
-    if(e.target && e.target.tagName === "I") {
-        e.target.classList.toggle("active");
-    }
-});
 
-elementListenerParent.addEventListener("mouseout", (e) => {
-    console.log(e);
-    if(e.target && e.target.tagName === "I"){
-        e.target.classList.toggle("active");
-    }
-})
+function turnOnOffStars(stars) {
+    const starClassOn = "star__icon fas fa-star";
+    const starClassOff = "star__icon far fa-star";
+    const starLength = stars.length;
+
+    container.addEventListener("click", (e) => {
+        if (e.target.className === "rating"){
+            for(element of ratingStars){
+                element.className = starClassOff;
+            }
+        }
+    })
+    let i;
+
+    stars.map((star) => {
+        star.addEventListener("click", () => {
+            i = stars.indexOf(star);
+            if (star.className === starClassOff) {
+                for (i; i < starLength; ++i) stars[i].className = starClassOn;
+            } else {
+                for (i; i >= 0; --i) stars[i].className = starClassOff;
+            }
+        })
+    });
+}
+
+
+turnOnOffStars(ratingStars);
